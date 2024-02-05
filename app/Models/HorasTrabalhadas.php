5cd031f770390141cd1962538a3c4578a248ed87
Gabriel Quina <gabriel.quina@ymail.com>
2024-02-05 02:21:44 -0300

    Calculos 99% finalizados - Falta entrada/saida noturna com virada de dia

--- app/Models/HorasTrabalhadas.php
+++ app/Models/HorasTrabalhadas.php
@@ -10,11 +10,9 @@ class HorasTrabalhadas extends Model
     public static function calcular(DateTime $horaEntrada, DateTime $horaSaida)
     {
         $horaInicioNoite = new DateTime("22:00:00");
+        $horaVirada = new DateTime("00:00:00");
+        $horaVirada->modify('+1 day');
         $horaFimNoite = new DateTime("05:00:00");
-
-        if($horaSaida < $horaEntrada){
-            $horaSaida->modify('+1 day');
-        };
 
         $entrouDia = $horaEntrada < $horaInicioNoite &&
                      $horaEntrada >= $horaFimNoite ? true : false;
@@ -22,12 +20,65 @@ class HorasTrabalhadas extends Model
         $saiuDia = $horaSaida <= $horaInicioNoite &&
                    $horaSaida > $horaFimNoite ? true : false;
 
-        $saiuNoite = $horaSaida < $horaFimNoite ||
-                     $horaSaida >= $horaInicioNoite ? true : false;
+        $saiuNoite = $horaSaida <= $horaFimNoite ||
+                     $horaSaida > $horaInicioNoite ? true : false;
 
-        //Dia trabalhado sem Hora Noturna
-        if ($entrouDia && $saiuDia) {
-            return $horaEntrada->diff($horaSaida)->format("%H:%I");
+        $virouData = $horaSaida < $horaEntrada ? true : false;
+
+        if($virouData){
+            $horaSaida->modify('+1 day');
+            $horaFimNoite->modify('+1 day');
+            $saiuDia = $horaSaida > $horaFimNoite->modify('+1 day') ? true : false;
+        };
+
+        if ($entrouDia) {
+            if ($saiuDia) {
+                if ($virouData){
+                    $horaDiurnaCalculada = $horaEntrada->sub($horaFimNoite->diff($horaSaida));
+                    $horaDiurnaCalculada = $horaEntrada->diff($horaInicioNoite)->format("%H:%I");
+                    $horaNoturnaCalculada = "07:00";
+
+                    return array("HorasDiurnas" => $horaDiurnaCalculada,
+                                 "HorasNoturnas" => $horaNoturnaCalculada);
+                }
+                $horaDiurnaCalculada = $horaEntrada->diff($horaSaida)->format("%H:%I");
+                $horaNoturnaCalculada = " ";
+                return array("HorasDiurnas" => $horaDiurnaCalculada,
+                            "HorasNoturnas" => $horaNoturnaCalculada);
+            }
+            if ($virouData){
+                $horaDiurnaCalculada = $horaEntrada->diff(($horaInicioNoite));
+                $horaNoturnaCalculada = $horaInicioNoite->diff($horaSaida);
+
+                return array("HorasDiurnas" => $horaDiurnaCalculada->format("%H:%I"),
+                             "HorasNoturnas" => $horaNoturnaCalculada->format("%H:%I"));
+            }
+            $horaDiurnaCalculada = $horaEntrada->diff($horaInicioNoite)->format("%H:%I");
+            $horaNoturnaCalculada = $horaInicioNoite->diff($horaSaida)->format("%H:%I");
+
+            return array("HorasDiurnas" => $horaDiurnaCalculada,
+                         "HorasNoturnas" => $horaNoturnaCalculada);
+        } else {
+            if ($saiuDia) {
+                $horaDiurnaCalculada = $horaFimNoite->diff($horaSaida)->format("%H:%I");
+                $horaNoturnaCalculada = $horaEntrada->diff($horaFimNoite)->format("%H:%I");
+
+                return array("HorasDiurnas" => $horaDiurnaCalculada,
+                "HorasNoturnas" => $horaNoturnaCalculada);
+            }
+            if ($virouData){
+                $horaDiurnaCalculada = "";
+                $horaNoturnaCalculada = $horaEntrada->add($horaFimNoite->diff($horaInicioNoite));
+                $horaNoturnaCalculada = $horaEntrada->diff($horaFimNoite)->format("%H:%I");
+
+                return array("HorasDiurnas" => "17:00",
+                "HorasNoturnas" => $horaNoturnaCalculada);
+            }
+            $horaDiurnaCalculada = $horaFimNoite->diff($horaSaida)->format("%H:%I");
+            $horaNoturnaCalculada = $horaEntrada->diff($horaSaida)->format("%H:%I");
+
+            return array("HorasDiurnas" => "",
+            "HorasNoturnas" => $horaNoturnaCalculada);
         }
     }
 }
